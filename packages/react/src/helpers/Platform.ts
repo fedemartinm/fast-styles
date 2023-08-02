4363217cb3ec358dcd200f8992863c8041364611
Fede Martin <fedemartin94@gmail.com>
2023-08-02 11:01:30 -0300

    final platform changes

--- packages/react/src/helpers/Platform.ts
+++ packages/react/src/helpers/Platform.ts
@@ -4,11 +4,17 @@
 import type { Platform } from 'react-native';
 
 let PlatformSelect: Platform['select'];
-
-if (typeof (globalThis as any).window !== 'undefined') {
-  PlatformSelect = (specify: any) => specify.web;
-} else {
+try {
   PlatformSelect = require('react-native').Platform.select;
+} catch (error) {
+  const message =
+    '⚠️ Fast Styles is designed to be used with React Native in mobile applications or on the web.' +
+    'However, it is written without direct dependencies on React Native, making it suitable for ' +
+    'use in a regular React app as well. If you want to use Fast Styles on the web without React Native' +
+    'and have good reasons to do so, please let us know at: https://github.com/fedemartinm/fast-styles/issues,' +
+    ' and we will improve the code to avoid the React Native warning.';
+  console.log(message);
+  PlatformSelect = (specify: any) => specify.web;
 }
 
 export { PlatformSelect };
